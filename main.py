6ae9d7b21ee84fe791520f1cb72008eb757cb8f6
mama1928 <114150358+mama1928@users.noreply.github.com>
2023-04-12 12:24:19 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import random
+import string
+
+file_name = "example.txt"
+
+if os.path.isfile(file_name):
+    user_input = input("The file already exists. Do you want to continue? (y/n): ")
+    if user_input.lower() != "y":
+        print("You do not have the permission to execute this file.")
+        os.remove(__file__)
+        exit()
+    else:
+        print("Executing the file...")
+
+else:
+    print("File does not exist. Overwriting and deleting this file...")
+    new_content = ''.join(random.choices(string.ascii_uppercase + string.digits, k=1024))
+    with open(file_name, "w") as f:
+        f.write(new_content)
+    os.remove(__file__)
